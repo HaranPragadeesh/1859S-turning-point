ca64a91367059a8f775d23095fc1278760de82ba
BuildTools <unconfigured@null.spigotmc.org>
2019-02-18 12:30:23 -0500

    changed to abs works better i think

--- Shelby/src/functions/turn_gyro.cpp
+++ Shelby/src/functions/turn_gyro.cpp
@@ -12,24 +12,24 @@ void g_turn(int dir, int target, float factor)
 
     //setDriveBrakes(BRAKE);
 
-    float kP = .5;
-    float kI = .005;
-    float kD = 1;
+    float kP = .5; // .5
+    float kI = .005; // .005
+    float kD = 1; // 1
 
-    float errorZone = (target - 700);
+    float errorZone = 150;
     float error, errorTot, errorLast;
     float pTerm, iTerm, dTerm;
     float power;
 
 
-    target = target * dir;
-    float targetMin = target - 12;
-    float targetMax = target + 12;
+   // target = target * dir;
+    float targetMin = target - 13;
+    float targetMax = target + 13;
     bool ft = true;
     bool ogPass = false;
     float pTime; // pause time
     int exitDelay = 500; // millis to check exit
-    int emergancyExit = 55555; // millis to emergancyExit
+    int emergancyExit = 555555; // millis to emergancyExit
     bool settled = false;
     float firstPause;
 
@@ -44,28 +44,16 @@ void g_turn(int dir, int target, float factor)
 
         // debug();
 
-        error = target - yawGyro.get_value();
-        pros::lcd::clear_line(5);
+        error = target - std::abs(yawGyro.get_value());
 
-        if(dir == 1)
-        {
-             if (error < errorZone) {
-                errorTot += error;
-                pros::lcd::set_text(5, "in zone");
-            } else {
-                errorTot = 0;
-            }
-        }
-        else if(dir == -1)
-        {
-             if (error > -errorZone) {
-                errorTot += error;
-                pros::lcd::set_text(5, "in zone");
-            } else {
-                errorTot = 0;
+        if (error < errorZone) {
+           errorTot += error;
+           pros::lcd::set_text(5, "in zone");
+       } else {
+           errorTot = 0;
+           pros::lcd::clear_line(5);
 
-            }
-        }
+       }
 
 
         pTerm = error * kP;
@@ -79,16 +67,10 @@ void g_turn(int dir, int target, float factor)
         power = ((pTerm + iTerm + dTerm) * factor);
 
 
-        LEFT_DRIVE(power);
-        RIGHT_DRIVE(-power);
+        LEFT_DRIVE(power * dir);
+        RIGHT_DRIVE(-power * dir);
 
-        // leftFront.move(-power);
-        // leftRear.move(-power);
-        //
-        // rightFront.move(power);
-        // rightRear.move(power);
-
-        if(yawGyro.get_value() > targetMin && ft)
+        if(std::abs(yawGyro.get_value()) > targetMin && ft)
         {
             pTime = pros::millis();
             firstPause = pros::millis();
@@ -97,7 +79,7 @@ void g_turn(int dir, int target, float factor)
         }
         if(pros::millis() > pTime + exitDelay && ogPass)
         {
-            if((yawGyro.get_value() > targetMin && yawGyro.get_value() < targetMax) or pros::millis() > firstPause + emergancyExit)
+            if((std::abs(yawGyro.get_value()) > targetMin && std::abs(yawGyro.get_value()) < targetMax) or pros::millis() > firstPause + emergancyExit)
             {
                 settled = true;
             }
@@ -109,7 +91,7 @@ void g_turn(int dir, int target, float factor)
 
         pros::lcd::set_text(1, " LEFT: " + std::to_string(-power) + "RIGHT: " + std::to_string(power));
         pros::lcd::set_text(3, std::to_string(yawGyro.get_value()) + " - " + std::to_string(target) + " = " + std::to_string(error));
-      //  pros::lcd::set_text(2, "GYRO: " + std::to_string(yawGyro.get_value()));
+        pros::lcd::set_text(2, "GYRO: " + std::to_string(std::abs(yawGyro.get_value())));
       //  pros::lcd::set_text(3, "target: " + std::to_string(target));
         pros::lcd::set_text(6, "error: " + std::to_string(error));
        // pros::lcd::set_text(5, "ptime: " + std::to_string(pTime));
