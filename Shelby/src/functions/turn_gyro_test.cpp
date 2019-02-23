53f3bdcad0ed1f6e05a03c4f6f94545fbed8eb7d
BuildTools <unconfigured@null.spigotmc.org>
2019-02-22 23:21:33 -0500

    more gyro stuff dont use right now need to integrate I value

--- Shelby/src/functions/turn_gyro_test.cpp
+++ Shelby/src/functions/turn_gyro_test.cpp
@@ -3,55 +3,62 @@
 #include "../v5setup.hpp"
 
 
-void g_turn_t(int dir, int target, float factor)
+#define YAW_VALUE yawGyro.get_value() * -1 // turning left should increase the value not decrease
+
+void g_turn_t(int target)
 {
      int aTarget = target - botAngle;
 
      yawGyro.reset();
 
+    float error, errorTot, errorLast;
+    float pTerm, iTerm, dTerm;
+
     float kP = .55; // .5
     float kI = .005; // .005
     float kD = 1; // 1
 
-    float errorZone = 150;
-    float error, errorTot, errorLast;
-    float pTerm, iTerm, dTerm;
-    float power;
-
-
     float targetMin = aTarget - 15;
     float targetMax = aTarget + 15;
+
+    float errorZone = 150; // figure out system to work both dir
+
+    float power;
+
+    // exit
     bool ft = true;
     bool ogPass = false;
+    bool settled = false;
+    // --
     float pTime; // pause time
+    float firstPause;
     int exitDelay = 500; // millis to check exit
     int emergancyExit = 5000000; // millis to emergancyExit
-    bool settled = false;
-    float firstPause;
 
 
-     while(!settled)
+    while(!settled)
     {
-        error = target - std::abs(yawGyro.get_value());
+        error = aTarget - YAW_VALUE;
 
-        if (error < errorZone) {
-           errorTot += error;
-       } else {
-           errorTot = 0;
-       }
+       // // figure out system for both directions
+       //  if (error < errorZone) {
+       //     errorTot += error;
+       // } else {
+       //     errorTot = 0;
+       // }
 
         pTerm = error * kP;
-
         iTerm = kI * errorTot;
         dTerm = kD * (error - errorLast);
+
         errorLast = error;
 
-        power = ((pTerm + iTerm + dTerm) * factor);
+        power = (pTerm + iTerm + dTerm);
 
-        LEFT_DRIVE(power * dir);
-        RIGHT_DRIVE(-power * dir);
+        LEFT_DRIVE(-power);
+        RIGHT_DRIVE(power);
 
-        if(std::abs(yawGyro.get_value()) > targetMin && ft)
+        if(YAW_VALUE > targetMin && ft)
         {
             pTime = pros::millis();
             firstPause = pros::millis();
@@ -60,32 +67,18 @@ void g_turn_t(int dir, int target, float factor)
         }
         if(pros::millis() > pTime + exitDelay && ogPass)
         {
-            if((std::abs(yawGyro.get_value()) > targetMin && std::abs(yawGyro.get_value()) < targetMax) or pros::millis() > firstPause + emergancyExit)
-            {
-                settled = true;
-            }
+            if((YAW_VALUE > targetMin && YAW_VALUE < targetMax) || pros::millis() > firstPause + emergancyExit)
+            { settled = true; }
             else
-            {
-                pTime = pros::millis();
-            }
+            { pTime = pros::millis();}
         }
-
         pros::Task::delay(20);
     }
 
+    botAngle = botAngle + YAW_VALUE;
     RIGHT_DRIVE(0);
     LEFT_DRIVE(0);
 
 	yawGyro.reset();
 
-}
-
-void g_left_t(int target, float factor)
-{
-  g_turn(LEFT, target, factor);
-}
-
-void g_right_t(int target, float factor)
-{
-  g_turn(RIGHT, target, factor);
 }
